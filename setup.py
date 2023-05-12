32b23f2b7f5ec4d68bc533dda8a0096086cbd0ab
mbilos <33008640+mbilos@users.noreply.github.com>
2023-05-12 17:16:24 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,31 +1,31 @@
 from setuptools import setup, find_packages
 
 install_requires = [
-    'numpy',
-    'pytest',
-    'scipy',
-    'torch',
-    'pytorch-lightning',
-    'torchdiffeq',
-    'torchsde',
-    'matplotlib',
-    'seaborn',
-    'pytorchts',
+    'numpy>=1.21.5',
+    'pytest>=6.2.4',
+    'scipy>=1.7.1',
+    'torch>=1.12.1',
+    'pytorch-lightning==1.6.0',
+    'torchdiffeq==0.2.3',
+    'torchsde==0.2.5',
+    'matplotlib>=3.4.3',
+    'seaborn==0.11.1',
+    'pytorchts==0.6.0',
     'gluonts==0.9.*',
-    'wget',
+    'wget==3.2',
 ]
 
 with open('README.md', 'r') as f:
     long_description = f.read()
 
 setup(name='tsdiff',
-      version='0.2.0',
+      version='0.1.0',
       description='Time series diffusion',
       long_description=long_description,
       long_description_content_type='text/markdown',
       url='',
       author='Marin Bilos',
-      author_email='marin.bilos@tum.de',
+      author_email='marin.bilos@morganstaley.com', # also: marin.bilos@tum.de
       packages=find_packages(),
       install_requires=install_requires,
       python_requires='>=3.7',
